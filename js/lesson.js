13801b5e3689669c04a6c1359a5c3e2ff23c7d7f
Дастан <dastanjanybekov92@gmail.com>
2024-03-16 13:20:12 +0600

    DZ3

--- js/lesson.js
+++ js/lesson.js
@@ -15,3 +15,72 @@ phoneButton.addEventListener('click', () =>{
         phoneSpan.style.color = 'red'
     }
 })
+
+// TAB SLIDER
+const tabContent = document.querySelectorAll('.tab_content_block');
+const tabs = document.querySelectorAll('.tab_content_item');
+const tabsParent = document.querySelector('.tab_content_items');
+let currentIndex = 0;
+let intervalId;
+
+const hideTabContent = () => {
+    tabContent.forEach((content) => {
+        content.style.display = 'none';
+    });
+    tabs.forEach((tab) => {
+        tab.classList.remove('tab_content_item_active');
+    });
+};
+
+const showTabContent = (index = 0) => {
+    tabContent[index].style.display = 'block';
+    tabs[index].classList.add('tab_content_item_active');
+};
+
+const nextTab = () => {
+    hideTabContent();
+    currentIndex = (currentIndex + 1) % tabContent.length;
+    showTabContent(currentIndex);
+};
+
+hideTabContent();
+showTabContent();
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabs.forEach((tab, tabIndex) => {
+            if (event.target === tab) {
+                hideTabContent();
+                currentIndex = tabIndex;
+                showTabContent(tabIndex);
+            }
+        });
+    }
+};
+
+
+const startAutoSlide = () => {
+    intervalId = setInterval(nextTab, 3000);
+};
+
+const stopAutoSlide = () => {
+    clearInterval(intervalId);
+};
+
+startAutoSlide();
+
+
+tabsParent.addEventListener('click', stopAutoSlide);
+
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', startAutoSlide);
+});
+
+
+
+
+
+
+
+
